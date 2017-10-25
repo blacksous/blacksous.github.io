51e6d48b90515a624bbba581bab132f6c509b0bf
patrick <patrick.betts@gmail.com>
2017-10-25 16:36:54 -0700

    Commented and cleaned up js.

--- resources/js/cbrisk.js
+++ resources/js/cbrisk.js
@@ -1,25 +1,23 @@
-function cBrisket() {
+function rBrisket() {
       var rtf = document.getElementById("rtf").value;
       var raw = 0;
-      var yld = 0.3621;
+      var yld = 0.3621; // Yield % converted to Decimal
 
-      raw += (rtf / yld) / 16;
+      raw += (rtf / yld) / 16; // Yield converted from wz to lbs
 
-      raw = Number((raw).toFixed(2));
+      raw = Number((raw).toFixed(2)); //Shortening to hundredths decimal place
 
       document.getElementById("raw").innerHTML = "Order " + raw + "# of Raw Brisket";
 }
 
-document.getElementById("rtf").addEventListener("change", cBrisket, false);
+document.getElementById("rtf").addEventListener("change", rBrisket, false);
 
 function fBrisket() {
   var rts = document.getElementById("rts").value;
   var svd = 0;
-  var yld = 0.6;
+  var yld = 0.6; // Yield % converted to Decimal
 
   svd += (rts / yld);
-
-  //rtf = Number((rtf).toFixed(1));
 
   document.getElementById("frtf").innerHTML = "You need" + svd + "oz of RTF Brisket";
 }
